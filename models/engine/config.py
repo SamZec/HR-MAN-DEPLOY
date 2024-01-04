c07212021b16cb349366bc9fa54f0a6d013477e8
SamZec <samzec4@gmail.com>
2024-01-04 17:51:25 +0000

    Upadte Database credentials

--- models/engine/config.py
+++ models/engine/config.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 
-databaseName = '/hrman'
+databaseName = '/hrman?replicaSet=489d92853e0f433f8c793d062d606122'
+user = 'hrmani:'
+password = 'hrman2024@'
 host_1 = 'iad2-c17-2.mongo.objectrocket.com:53259,'
 host_2 = 'iad2-c17-0.mongo.objectrocket.com:53259,'
 host_3 = 'iad2-c17-1.mongo.objectrocket.com:53259'
-host = host_1 + host_2 + host_3 + databaseName
+host = user + password +  host_1 + host_2 + host_3 + databaseName
